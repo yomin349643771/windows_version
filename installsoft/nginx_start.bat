e1e88b5a69bd366b437d3f47b3d244b54a512416
yomin349643771 <59046805+yomin349643771@users.noreply.github.com>
2020-01-06 15:18:24 +0900

    Add files via upload

--- installsoft/nginx_start.bat
+++ installsoft/nginx_start.bat
@@ -1,4 +1,4 @@
 cd zip
-unzip.exe ..\nginx-1.16.0.zip -d %USERPROFILE%\Downloads\aceweb_basic-master\windows_ace\installsoft
+unzip.exe ..\nginx-1.16.0.zip -d %USERPROFILE%\Downloads\windows_version-master\installsoft
 cd ..
 ren nginx-1.16.0 nginx
